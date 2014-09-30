d7d92cdfaa9170b7c5e66f0bab144b59eeeb2fa7
Christophe Thiéry <christopho128@gmail.com>
2014-09-30 12:48:38 +0200

    Show a directory chooser dialog to load a quest.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,30 +1,48 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 #include <solarus/Arguments.h>
 #include <solarus/MainLoop.h>
 #include <QFileDialog>
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow(QWidget* parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
 
+    // Add menu actions to this widget so that key shortcuts work
+    // when menus are closed.
     addAction(ui->actionNew_quest);
     addAction(ui->actionLoad_quest);
     addAction(ui->actionExit);
     addAction(ui->actionRun_quest);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+QString MainWindow::get_quest_path() {
+    return quest_path;
 }
 
-void MainWindow::on_actionRun_quest_triggered()
-{
-    // TODO run the open quest instead
-    QFileDialog dialog(nullptr, tr("Choose quest directory"));
+void MainWindow::set_quest_path(QString quest_path) {
+    this->quest_path = quest_path;
+}
+
+void MainWindow::on_actionRun_quest_triggered() {
+    if (quest_path.isEmpty()) {
+        return;
+    }
+
+    // TODO run quest in a separate thread
+    Solarus::Arguments arguments;
+    arguments.add_argument(quest_path.toStdString());
+    Solarus::MainLoop main_loop(arguments);
+    main_loop.run();
+}
+
+void MainWindow::on_actionExit_triggered() {
+    std::exit(0);
+}
+
+void MainWindow::on_actionLoad_quest_triggered() {
+    QFileDialog dialog(nullptr, tr("Select quest directory"));
     dialog.setFileMode(QFileDialog::Directory);
     dialog.setOption(QFileDialog::ShowDirsOnly);
     if (!dialog.exec()) {
@@ -35,16 +53,6 @@ void MainWindow::on_actionRun_quest_triggered()
     if (file_names.empty()) {
         return;
     }
-    dialog.close();
 
-    // TODO run quest in a separate thread
-    Solarus::Arguments arguments;
-    arguments.add_argument(file_names.first().toStdString());
-    Solarus::MainLoop main_loop(arguments);
-    main_loop.run();
-}
-
-void MainWindow::on_actionExit_triggered()
-{
-    std::exit(0);
+    set_quest_path(file_names.first());
 }
