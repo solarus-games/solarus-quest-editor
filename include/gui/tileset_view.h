5fab048bc4290cff47f34dc3ca263f28b3a1e0bd
Christopho <christopho@solarus-games.org>
2014-10-15 11:52:31 +0200

    Starting the tileset main view.

---
+++ include/gui/tileset_view.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 Christopho, Solarus - http://www.solarus-games.org
+ *
+ * Solarus Quest Editor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Solarus Quest Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SOLARUSEDITOR_TILESET_VIEW_H
+#define SOLARUSEDITOR_TILESET_VIEW_H
+
+#include <QGraphicsView>
+
+/**
+ * @brief TODO
+ */
+class TilesetView : public QGraphicsView {
+
+public:
+
+  TilesetView(QWidget* parent = nullptr);
+
+};
+
+#endif
