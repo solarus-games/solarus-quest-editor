9411724c05e74e178d389879db17bf3796a08d2c
Christopho <christopho@solarus-games.org>
2014-11-02 19:03:00 +0100

    Starting a dialog that runs a Lua script and shows its output.

---
+++ include/gui/external_script_dialog.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 Christopho, Solarus - http://www.solarus-games.org
+ *
+ * Solarus Quest Editor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Solarus Quest Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef EXTERNAL_SCRIPT_DIALOG_H
+#define EXTERNAL_SCRIPT_DIALOG_H
+
+#include "ui_external_script_dialog.h"
+#include <QDialog>
+
+/**
+ * @brief A dialog that runs an external Lua script and shows its output.
+ */
+class ExternalScriptDialog : public QDialog {
+  Q_OBJECT
+
+public:
+
+  ExternalScriptDialog(const QString& title, const QString& file_name, QWidget* parent = 0);
+
+private:
+
+  Ui::ExternalScriptDialog ui;
+
+};
+
+#endif
---
+++ src/gui/external_script_dialog.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 Christopho, Solarus - http://www.solarus-games.org
+ *
+ * Solarus Quest Editor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Solarus Quest Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "gui/external_script_dialog.h"
+
+/**
+ * Creates a script dialog.
+ * @param title Title of the dialog box.
+ * @param file_name The script to run, without extension.
+ * @param arg An argument to pass to the script, or an empty string.
+ * @param parent The parent object or nullptr.
+ */
+ExternalScriptDialog::ExternalScriptDialog(
+    const QString& title, const QString& file_name, QWidget* parent) {
+
+  // TODO
+  Q_UNUSED(title);
+  Q_UNUSED(file_name);
+  Q_UNUSED(parent);
+}
