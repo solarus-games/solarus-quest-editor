230e8e964b74c944047a86592710cbd32ead81e2
Christopho <christopho@solarus-games.org>
2015-01-22 16:40:24 +0100

    Add helpers about entity types.

---
+++ include/entity_traits.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2014-2015 Christopho, Solarus - http://www.solarus-games.org
+ *
+ * Solarus Quest Editor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Solarus Quest Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SOLARUSEDITOR_ENTITY_TRAITS_H
+#define SOLARUSEDITOR_ENTITY_TRAITS_H
+
+#include "enum_traits.h"
+#include <solarus/entities/EntityType.h>
+
+using EntityType = Solarus::EntityType;
+using EntityTraits = EnumTraits<EntityType>;
+
+/**
+ * \brief Provides useful properties of entity type enum values.
+ */
+template<>
+class EnumTraits<EntityType> {
+
+public:
+
+  static QList<EntityType> get_values();
+  static QString get_friendly_name(EntityType value);
+  static QIcon get_icon(EntityType value);
+
+  static QString get_lua_name(EntityType value);
+
+};
+
+#endif
---
+++ src/entity_traits.cpp
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2014-2015 Christopho, Solarus - http://www.solarus-games.org
+ *
+ * Solarus Quest Editor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Solarus Quest Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "entity_traits.h"
+#include <solarus/entities/EntityTypeInfo.h>
+#include <QApplication>
+
+/**
+ * @brief Returns all values.
+ * @return The existing values.
+ */
+QList<EntityType> EnumTraits<EntityType>::get_values() {
+
+  QList<EntityType> types;
+  for (const auto& kvp : Solarus::EntityTypeInfo::get_entity_type_names()) {
+    EntityType type = kvp.first;
+    types.append(type);
+  }
+  return types;
+}
+
+/**
+ * @brief Returns a user-friendly name describing a value.
+ * @param value A value.
+ * @return The human-readable name of this value in the current language.
+ */
+QString EnumTraits<EntityType>::get_friendly_name(EntityType value) {
+
+  // Use a switch to ensure we don't forget a value,
+  // and also to translate names dynamically.
+  switch (value) {
+
+  case EntityType::ARROW:
+    return QApplication::tr("Arrow");
+
+  case EntityType::BLOCK:
+    return QApplication::tr("Block");
+
+  case EntityType::BOMB:
+    return QApplication::tr("Bomb");
+
+  case EntityType::BOOMERANG:
+    return QApplication::tr("Boomerang");
+
+  case EntityType::CARRIED_ITEM:
+    return QApplication::tr("Carried object");
+
+  case EntityType::CHEST:
+    return QApplication::tr("Chest");
+
+  case EntityType::CRYSTAL:
+    return QApplication::tr("Crystal");
+
+  case EntityType::CRYSTAL_BLOCK:
+    return QApplication::tr("Crystal block");
+
+  case EntityType::CUSTOM:
+    return QApplication::tr("Custom entity");
+
+  case EntityType::DESTINATION:
+    return QApplication::tr("Destination");
+
+  case EntityType::DESTRUCTIBLE:
+    return QApplication::tr("Destructible object");
+
+  case EntityType::DOOR:
+    return QApplication::tr("Door");
+
+  case EntityType::DYNAMIC_TILE:
+    return QApplication::tr("Dynamic tile");
+
+  case EntityType::ENEMY:
+    return QApplication::tr("Enemy");
+
+  case EntityType::EXPLOSION:
+    return QApplication::tr("Explosion");
+
+  case EntityType::FIRE:
+    return QApplication::tr("Fire");
+
+  case EntityType::HERO:
+    return QApplication::tr("Hero");
+
+  case EntityType::HOOKSHOT:
+    return QApplication::tr("Hookshot");
+
+  case EntityType::JUMPER:
+    return QApplication::tr("Jumper");
+
+  case EntityType::NPC:
+    return QApplication::tr("NPC");
+
+  case EntityType::PICKABLE:
+    return QApplication::tr("Pickable treasure");
+
+  case EntityType::SENSOR:
+    return QApplication::tr("Sensor");
+
+  case EntityType::SEPARATOR:
+    return QApplication::tr("Separator");
+
+  case EntityType::SHOP_TREASURE:
+    return QApplication::tr("Shop treasure");
+
+  case EntityType::STAIRS:
+    return QApplication::tr("Stairs");
+
+  case EntityType::STREAM:
+    return QApplication::tr("Stream");
+
+  case EntityType::SWITCH:
+    return QApplication::tr("Switch");
+
+  case EntityType::TELETRANSPORTER:
+    return QApplication::tr("Teletransporter");
+
+  case EntityType::TILE:
+    return QApplication::tr("Tile");
+
+  case EntityType::WALL:
+    return QApplication::tr("Wall");
+
+  }
+
+  return "";
+}
+
+/**
+ * @brief Returns an icon representing a value.
+ * @param value A value.
+ * @return The corresponding icon.
+ */
+QIcon EnumTraits<EntityType>::get_icon(EntityType value) {
+  return QIcon(":/images/entity_" + get_lua_name(value) + ".png");
+}
+
+/**
+ * @brief Returns the Lua name of a value.
+ * @param value A value.
+ * @return The corresponding Lua name.
+ */
+QString EnumTraits<EntityType>::get_lua_name(EntityType value) {
+  return QString::fromStdString(Solarus::EntityTypeInfo::get_entity_type_name(value));
+}
