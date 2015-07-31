651eb8851912f086a4729a20e212e4d43a064ebc
Christopho <christopho@solarus-games.org>
2015-07-31 15:43:44 +0200

    Use the cmake target for install

--- cmake/modules/FindSolarus.cmake
+++ cmake/modules/FindSolarus.cmake
@@ -4,27 +4,21 @@
 #  SOLARUS_INCLUDE_DIR - where to find solarus headers.
 #  SOLARUS_LIBRARIES   - List of libraries when using libsolarus.
 #  SOLARUS_FOUND       - True if Solarus was found.
-
-if(SOLARUS_INCLUDE_DIR)
-  # Already in cache, be silent
-  set(SOLARUS_FIND_QUIETLY TRUE)
-endif(SOLARUS_INCLUDE_DIR)
-
 find_path(SOLARUS_INCLUDE_DIR solarus/MainLoop.h)
 
-find_library(SOLARUS_LIBRARY NAMES solarus)
+find_library(SOLARUS_LIBRARY solarus)
 
 # Handle the QUIETLY and REQUIRED arguments and set SOLARUS_FOUND to TRUE if
 # all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SOLARUS DEFAULT_MSG
-  SOLARUS_INCLUDE_DIR SOLARUS_LIBRARY)
+  SOLARUS_LIBRARY SOLARUS_INCLUDE_DIR)
 
 if(SOLARUS_FOUND)
   set(SOLARUS_LIBRARIES ${SOLARUS_LIBRARY})
-else(SOLARUS_FOUND)
+else()
   set(SOLARUS_LIBRARIES)
-endif(SOLARUS_FOUND)
+endif()
 
 mark_as_advanced(SOLARUS_INCLUDE_DIR SOLARUS_LIBRARY)
 
