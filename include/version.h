050c40bc74728cc47382fed33124a9168e33f601
Christopho <christopho@solarus-games.org>
2015-08-20 12:59:33 +0200

    Development version 1.4.4

--- include/version.h
+++ include/version.h
@@ -36,7 +36,7 @@
  * Patch versions are only bug fixes.
  * No need to match the Solarus library patch version.
  */
-#define SOLARUSEDITOR_PATCH_VERSION 3
+#define SOLARUSEDITOR_PATCH_VERSION 4
 
 /**
  * @brief The Solarus Quest Editor version as a string.
