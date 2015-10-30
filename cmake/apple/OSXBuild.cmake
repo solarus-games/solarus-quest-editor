33784387dfc40c9d14d8c707843072c9ac06a65c
vlag <valentin.soudier@gmail.com>
2015-10-30 15:13:20 +0100

    Fix OSX segfault with LuaJIT

--- cmake/apple/OSXBuild.cmake
+++ cmake/apple/OSXBuild.cmake
@@ -25,23 +25,6 @@ if(NOT SOLARUS_ARCH)
 endif()
 set(CMAKE_OSX_ARCHITECTURES "${SOLARUS_ARCH}" CACHE STRING "Build architecture" FORCE)
 
-# SDK version.
-if(NOT SOLARUS_SYSROOT)
-  # Default SDKs locations, from the earlier naming convention to the older one.
-  if(EXISTS /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_SHORT}.sdk)
-    set(SOLARUS_SYSROOT "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_SHORT}.sdk")
-  elseif(EXISTS /Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_SHORT}.sdk)
-    set(SOLARUS_SYSROOT "/Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_SHORT}.sdk")
-  elseif(EXISTS /Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_SHORT}u.sdk)
-    set(SOLARUS_SYSROOT "/Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_SHORT}u.sdk")
-  elseif(EXISTS /Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_LONG}.sdk)
-    set(SOLARUS_SYSROOT "/Developer/SDKs/MacOSX${SOLARUS_CURRENT_OSX_VERSION_LONG}.sdk")
-  else()
-    message(WARNING "No ${SOLARUS_CURRENT_OSX_VERSION_SHORT} or ${SOLARUS_CURRENT_OSX_VERSION_LONG} SDK found, force deployment version to default.")
-  endif()
-endif()
-set(CMAKE_OSX_SYSROOT "${SOLARUS_SYSROOT}" CACHE STRING "Build SDK" FORCE)
-
 # Deployment version.
 if(NOT SOLARUS_DEPLOYMENT AND DEFINED SOLARUS_SYSROOT)
   set(SOLARUS_DEPLOYMENT "10.7")
@@ -58,3 +41,15 @@ set_target_properties(solarus-quest-editor PROPERTIES
   BUILD_WITH_INSTALL_RPATH           1
   INSTALL_NAME_DIR                   "@rpath"
 )
+
+# LuaJIT workaround.
+# According to LuaJIT doc, OSX needs to link with additional flags if 64bit build is requested
+if(SOLARUS_USE_LUAJIT AND SOLARUS_ARCH MATCHES "x86_64")
+  if(XCODE)
+    set_property(TARGET solarus PROPERTY
+      "XCODE_ATTRIBUTE_LINKER_FLAGS[arch=x86_64]" "-pagezero_size 10000 -image_base 100000000"
+    )
+  elseif(NOT CMAKE_EXE_LINKER_FLAGS MATCHES "-pagezero_size 10000 -image_base 100000000")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pagezero_size 10000 -image_base 100000000" CACHE STRING "LuaJIT woraround" FORCE)
+  endif()
+endif()
