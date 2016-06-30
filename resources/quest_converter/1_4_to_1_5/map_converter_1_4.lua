48f4b5f6bcb6dddc3ceae06603f25cee3bb8a68b
Christopho <christopho@solarus-games.org>
2016-06-30 13:07:55 +0200

    Fix custom entity width and height that were optional by mistake
    Closes #169.
    
    Users who use the development version of 1.5 should re-convert all their
    maps from 1.4 to 1.5. To do so:
    - Make a backup of your quest.
    - Manually set solarus_version to "1.4" in quest.dat with a text editor.
    - Open your quest in the editor: the editor will convert to 1.5 again
      to the correct format.
    
    (Don't worry, whatever was already converted before will be preserved).

--- resources/quest_converter/1_4_to_1_5/map_converter_1_4.lua
+++ resources/quest_converter/1_4_to_1_5/map_converter_1_4.lua
@@ -2,6 +2,8 @@
 -- Changes in the map syntax:
 -- - New mandatory properties min_layer and max_layer.
 -- - The rank property of enemies no longer exists.
+-- - The width and height of custom entities are now mandatory
+--   (it was a bug that they were not).
 
 local converter = {}
 
@@ -16,15 +18,27 @@ function converter.convert(quest_path, map_id, default_font_id)
   local text = input_file:read("*a")  -- Read the whole file.
 
   -- Add the min_layer and max_layer properties.
-  text = text:gsub(
-      "\n  tileset = \"",
-      "\n  min_layer = 0,\n  max_layer = 2,\n  tileset = \""
-  )
+  if not text:match("\n  min_layer = ") then
+    text = text:gsub(
+        "\n  tileset = \"",
+        "\n  min_layer = 0,\n  max_layer = 2,\n  tileset = \""
+    )
+  end
 
   -- Remove the rank property of enemies.
   text = text:gsub(
       "\n  rank = [0-2],",
       ""
+  )
+
+  -- Add width and height to custom entities that have none.
+  text = text:gsub(
+      "(\ncustom_entity{\n  layer = [0-2],\n  x = [-0-9]+,\n  y = [-0-9]+,)\n  direction = ",
+      "%1\n  width = 16,\n  height = 16,\n  direction = "
+  )
+  text = text:gsub(
+      "(\ncustom_entity{\n  name = \"[^\"\n]*\",\n  layer = [0-2],\n  x = [-0-9]+,\n  y = [-0-9]+,)\n  direction = ",
+      "%1\n  width = 16,\n  height = 16,\n  direction = "
   )
 
   input_file:close()
--- resources/quest_converter/1_4_to_1_5/quest_properties_converter_1_4.lua
+++ resources/quest_converter/1_4_to_1_5/quest_properties_converter_1_4.lua
@@ -31,11 +31,29 @@ function converter.convert(quest_path)
   output_file:write("  solarus_version = \"1.5\",\n")
 
   -- Initialize the quest title from the obsolete title_bar value.
-  local title = properties.title_bar or "Untitled quest"
-  output_file:write("  title = \"" .. title .. "\",\n")
+  local title = properties.title_bar or properties.title or "Untitled quest"
 
   if properties.write_dir ~= nil then
     output_file:write("  write_dir = \"" .. properties.write_dir .. "\",\n")
+  end
+  output_file:write("  title = \"" .. title .. "\",\n")
+  if properties.short_description ~= nil then
+    output_file:write("  short_description = \"" .. properties.short_description .. "\",\n")
+  end
+  if properties.long_description ~= nil then
+    output_file:write("  long_description = [[\n" .. properties.long_description .. "]],\n")
+  end
+  if properties.author ~= nil then
+    output_file:write("  author = \"" .. properties.author .. "\",\n")
+  end
+  if properties.quest_version ~= nil then
+    output_file:write("  quest_version = \"" .. properties.quest_version .. "\",\n")
+  end
+  if properties.release_date ~= nil then
+    output_file:write("  release_date = \"" .. properties.release_date .. "\",\n")
+  end
+  if properties.website ~= nil then
+    output_file:write("  website = \"" .. properties.website .. "\",\n")
   end
   if properties.normal_quest_size ~= nil then
     output_file:write("  normal_quest_size = \"" .. properties.normal_quest_size .. "\",\n")
