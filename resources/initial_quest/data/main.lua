fa67f66ddaa73587df26617a750bc0f082eb167c
Christopho <christopho@solarus-games.org>
2016-07-22 03:40:29 +0200

    Initial quest: add keyboard keys to switch the video modes

--- resources/initial_quest/data/main.lua
+++ resources/initial_quest/data/main.lua
@@ -22,3 +22,28 @@ function sol.main:on_started()
 
 end
 
+-- Event called when the player pressed a keyboard key.
+function sol.main:on_key_pressed(key, modifiers)
+
+  local handled = false
+  if key == "f5" then
+    -- F5: change the video mode.
+    sol.video.switch_mode()
+    handled = true
+  elseif key == "f11" or
+    (key == "return" and (modifiers.alt or modifiers.control)) then
+    -- F11 or Ctrl + return or Alt + Return: switch fullscreen.
+    sol.video.set_fullscreen(not sol.video.is_fullscreen())
+    handled = true
+  elseif key == "f4" and modifiers.alt then
+    -- Alt + F4: stop the program.
+    sol.main.exit()
+    handled = true
+  elseif key == "escape" and sol.main.game == nil then
+    -- Escape in title screens: stop the program.
+    sol.main.exit()
+    handled = true
+  end
+
+  return handled
+end
