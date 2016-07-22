dcb5e0e2086f28b1a46ff1760958f199ea9dc763
Christopho <christopho@solarus-games.org>
2016-07-22 03:26:37 +0200

    Add a game manager script to the initial quest to make it playable

--- resources/initial_quest/data/main.lua
+++ resources/initial_quest/data/main.lua
@@ -1,14 +1,11 @@
 -- This is the main Lua script of your project.
 -- You will probably make a title screen and then start a game.
--- See the Lua API! http://www.solarus-games.org/solarus/documentation/
+-- See the Lua API! http://www.solarus-games.org/doc/latest
 
--- This is just an example of quest that shows the Solarus logo
--- and then does nothing.
--- Feel free to change this!
+local game_manager = require("scripts/game_manager")
 
+-- This function is called when Solarus starts.
 function sol.main:on_started()
-  -- This function is called when Solarus starts.
-  print("This is a sample quest for Solarus.")
 
   -- Setting a language is useful to display text and dialogs.
   sol.language.set_language("en")
@@ -17,9 +14,10 @@ function sol.main:on_started()
 
   -- Show the Solarus logo initially.
   sol.menu.start(self, solarus_logo)
+
+  -- Start the game when the Solarus logo menu is finished.
   solarus_logo.on_finished = function()
-    -- Do whatever you want next, like showing your title screen
-    -- or starting a game.
+    game_manager:start_game()
   end
 
 end
