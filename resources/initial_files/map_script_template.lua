f39b97976ed8d1c4ad074d99501f7c082ad19c1b
Christopho <christopho@solarus-games.org>
2017-07-12 09:14:07 +0200

    Typo

--- resources/initial_files/map_script_template.lua
+++ resources/initial_files/map_script_template.lua
@@ -10,7 +10,7 @@
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map becomes is loaded.
+-- Event called at initialization time, as soon as this map is loaded.
 function map:on_started()
 
   -- You can initialize the movement and sprites of various
