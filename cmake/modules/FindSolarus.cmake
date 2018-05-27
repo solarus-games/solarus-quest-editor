71e2d1b9719f19392df6647d340ee2f97bf60752
Christopho <christopho@solarus-games.org>
2018-05-27 02:46:13 -0700

    Update windows compilation

--- cmake/modules/FindSolarus.cmake
+++ cmake/modules/FindSolarus.cmake
@@ -2,24 +2,21 @@
 # Find the Solarus includes and library.
 # Does not look for the Solarus GUI library.
 #
-#  SOLARUS_INCLUDE_DIR - where to find solarus headers.
-#  SOLARUS_LIBRARIES   - List of libraries when using libsolarus.
-#  SOLARUS_FOUND       - True if Solarus was found.
+#  SOLARUS_INCLUDE_DIRS - where to find solarus headers.
+#  SOLARUS_LIBRARIES    - List of libraries when using libsolarus.
+#  SOLARUS_FOUND        - True if Solarus was found.
 find_path(SOLARUS_INCLUDE_DIR solarus/core/MainLoop.h)
 
 find_library(SOLARUS_LIBRARY solarus)
 
-# Handle the QUIETLY and REQUIRED arguments and set SOLARUS_FOUND to TRUE if
-# all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SOLARUS DEFAULT_MSG
-  SOLARUS_LIBRARY SOLARUS_INCLUDE_DIR)
+find_package_handle_standard_args(SOLARUS
+    FOUND_VAR SOLARUS_FOUND
+    REQUIRED_VARS
+        SOLARUS_INCLUDE_DIR
+        SOLARUS_LIBRARY)
 
 if(SOLARUS_FOUND)
-  set(SOLARUS_LIBRARIES ${SOLARUS_LIBRARY})
-else()
-  set(SOLARUS_LIBRARIES)
+    set(SOLARUS_INCLUDE_DIRS ${SOLARUS_INCLUDE_DIR})
+    set(SOLARUS_LIBRARIES ${SOLARUS_LIBRARY})
 endif()
-
-mark_as_advanced(SOLARUS_INCLUDE_DIR SOLARUS_LIBRARY)
-
--- cmake/modules/FindSolarusGui.cmake
+++ cmake/modules/FindSolarusGui.cmake
@@ -1,24 +1,21 @@
 # - Find Solarus
 # Find the Solarus GUI includes and library.
 #
-#  SOLARUS_GUI_INCLUDE_DIR - where to find solarus headers.
-#  SOLARUS_GUI_LIBRARIES   - List of libraries when using libsolarus.
-#  SOLARUS_GUI_FOUND       - True if Solarus was found.
+#  SOLARUS_GUI_INCLUDE_DIRS - where to find solarus GUI headers.
+#  SOLARUS_GUI_LIBRARIES    - List of libraries when using libsolarus-gui.
+#  SOLARUS_GUI_FOUND        - True if Solarus GUI was found.
 find_path(SOLARUS_GUI_INCLUDE_DIR solarus/gui/main_window.h)
 
 find_library(SOLARUS_GUI_LIBRARY solarus-gui)
 
-# Handle the QUIETLY and REQUIRED arguments and set SOLARUS_GUI_FOUND to TRUE if
-# all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SOLARUS_GUI DEFAULT_MSG
-  SOLARUS_GUI_LIBRARY SOLARUS_GUI_INCLUDE_DIR)
+find_package_handle_standard_args(SOLARUS_GUI
+    FOUND_VAR SOLARUS_GUI_FOUND
+    REQUIRED_VARS
+        SOLARUS_GUI_INCLUDE_DIR
+        SOLARUS_GUI_LIBRARY)
 
 if(SOLARUS_GUI_FOUND)
-  set(SOLARUS_GUI_LIBRARIES ${SOLARUS_GUI_LIBRARY})
-else()
-  set(SOLARUS_GUI_LIBRARIES)
+    set(SOLARUS_GUI_INCLUDE_DIRS ${SOLARUS_GUI_INCLUDE_DIR})
+    set(SOLARUS_GUI_LIBRARIES ${SOLARUS_GUI_LIBRARY})
 endif()
-
-mark_as_advanced(SOLARUS_GUI_INCLUDE_DIR SOLARUS_GUI_LIBRARY)
-
